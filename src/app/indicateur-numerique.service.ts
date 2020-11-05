a9370961bf985c7b71bd24eab3b0cc02da3273a0
Benjamin Valognes <hello@benjv.fr>
2020-11-05 16:18:06 +0100

    Ajout variable environnement

--- src/app/indicateur-numerique.service.ts
+++ src/app/indicateur-numerique.service.ts
@@ -1,15 +1,14 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { IndicateurNumerique } from './model/indicateur-numerique';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IndicateurNumeriqueService {
-
-  private endpoint: string = "http://localhost:8080"
-
+  
   constructor(
     private http: HttpClient
   ) {}
@@ -17,7 +16,7 @@ export class IndicateurNumeriqueService {
   public getIndicateur(commune: string): Observable<IndicateurNumerique|HttpErrorResponse> {
 
     return new Observable<IndicateurNumerique|HttpErrorResponse>(observer => {
-      this.http.get(this.endpoint + "/commune?nom=" + commune).subscribe(
+      this.http.get(environment.apiEndpoint + "/commune?nom=" + commune).subscribe(
         (data: IndicateurNumerique) => observer.next(data),
         (err: HttpErrorResponse) => observer.next(err)
       );
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiEndpoint: "http://vps-b2dae177.vps.ovh.net:8080/api"
 };
