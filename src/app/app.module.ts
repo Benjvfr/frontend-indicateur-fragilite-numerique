5381149b5ea02e8b20d37966c6523d7c87049642
Benjamin Valognes <hello@benjv.fr>
2020-11-05 09:25:06 +0100

    Ajout du sub-header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SubHeaderComponent } from './sub-header/sub-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SubHeaderComponent
   ],
   imports: [
     BrowserModule
